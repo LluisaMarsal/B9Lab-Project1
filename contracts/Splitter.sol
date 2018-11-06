7b4b94addd4ddfb23e6e65596c3f13a825f81a90
LluisaMarsal <44668677+LluisaMarsal@users.noreply.github.com>
2018-11-06 15:54:21 +0100

    last updates after Xavier's comments

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -2,17 +2,15 @@ pragma solidity ^0.4.19;
 
 contract Splitter {
     address public owner; 
-    uint256 sendAmount;
 
     function Splitter() public payable {
         owner = msg.sender; 
-        sendAmount = msg.value/2;
-        msg.value % 2 == 0; 
     }
    
-    function send (address  recipient1, address  recipient2) public payable {
-        recipient1.transfer(msg.value/2);
-        recipient2.transfer(msg.value/2);
+    function transferSharedEther (address  recipient1, address  recipient2) public payable {
+        uint amountToSend = msg.value/2;
+        recipient1.transfer(amountToSend);
+        recipient2.transfer(amountToSend);
     } 
 
     function killMe() public returns (bool) {
