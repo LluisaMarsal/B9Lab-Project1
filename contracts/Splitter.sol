73f62fd1998c9cfa9e7e6b0243d420deb644eacc
LluisaMarsal <llmarsal@intelligenter.org>
2018-11-07 19:53:56 +0100

    added withdraw function

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -1,13 +1,20 @@
 pragma solidity ^0.4.19;
 
 contract Splitter {
-    address public owner; 
+    address public owner;
+    mapping (address => uint) pendingWithdrawals; 
 
     function Splitter() public payable {
         owner = msg.sender; 
     }
    
-    function transferSharedEther (address  recipient1, address  recipient2) public payable {
+    function withdraw() public payable { 
+        uint amount = pendingWithdrawals[msg.sender];
+        pendingWithdrawals[msg.sender] = 0;
+        msg.sender.transfer(amount);
+    }
+
+    function transferSharedEther(address  recipient1, address  recipient2) public {
         require (msg.value % 2 == 0);
         uint amountToSend = msg.value/2;
         recipient1.transfer(amountToSend);
