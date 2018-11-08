2174cd707684e28a3298cdb443141fc922761bd5
LluisaMarsal <llmarsal@intelligenter.org>
2018-11-08 19:46:08 +0100

    revised withdraw function

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -4,21 +4,20 @@ contract Splitter {
     address public owner;
     mapping (address => uint) pendingWithdrawals; 
 
-    function Splitter() public payable {
+    function Splitter() public {
         owner = msg.sender; 
     }
    
-    function withdraw() public payable { 
+    function withdraw() public { 
         uint amount = pendingWithdrawals[msg.sender];
-        pendingWithdrawals[msg.sender] = 0;
-        msg.sender.transfer(amount);
+        pendingWithdrawals[msg.sender] >= 0;
     }
 
-    function transferSharedEther(address  recipient1, address  recipient2) public {
+    function transferSharedEther(address  recipient1, address  recipient2) public payable {
         require (msg.value % 2 == 0);
         uint amountToSend = msg.value/2;
-        recipient1.transfer(amountToSend);
-        recipient2.transfer(amountToSend);
+        recipient1.pendingWithdrawals(amountToSend);
+        recipient2.pendingWithdrawals(amountToSend);
     } 
 
     function killMe() public returns (bool) {
