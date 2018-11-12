abafbd2141c40493fdb3f167ad6937baeacceef5
LluisaMarsal <llmarsal@intelligenter.org>
2018-11-12 19:20:11 +0100

    reentrance prevention

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -11,20 +11,23 @@ contract Splitter {
     function withdraw() public { 
         uint amount = pendingWithdrawals[msg.sender];
         require (amount >= 0);
+        pendingWithdrawals[msg.sender] = 0; 
         msg.sender.transfer(amount);
-    }
-
-    function update(uint remainingBalance) private returns (uint) {
-        pendingWithdrawals[msg.sender] = remainingBalance;
-        return remainingBalance;
     }
 
     function transferSharedEther(address  recipient1, address  recipient2) public payable {
         require (msg.value % 2 == 0);
-        uint amountToSend = msg.value/2;
-        pendingWithdrawals[recipient1] = amountToSend;
-        pendingWithdrawals[recipient2] = amountToSend;
+        uint amountToSend1 = msg.value/2;
+        uint amountToSend2 = msg.value/2;
+        pendingWithdrawals[recipient1] = amountToSend1;
+        pendingWithdrawals[recipient2] = amountToSend2;
+        amountToSend1 + amountToSend2 == msg.value; 
     } 
+    
+    function update(uint remainingBalance) private returns (uint) {
+        pendingWithdrawals[msg.sender] = remainingBalance;
+        return remainingBalance;
+    }
 
     function killMe() public returns (bool) {
         require(msg.sender == owner);
