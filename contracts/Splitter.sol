fb86c3b06a03530089ecbea332dfc9b782225edb
LluisaMarsal <llmarsal@intelligenter.org>
2018-11-14 22:27:20 +0100

    events and change ownership

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -2,8 +2,12 @@ pragma solidity ^0.4.19;
 
 contract Splitter {
     address public owner;
+    address onlyOwner = owner;
     mapping (address => uint) pendingWithdrawals; 
 
+    event LogWithdrawl(address indexed owner, uint amount);
+    event LogTransferSharedEther(address indexed recipient1, address indexed recipient2, uint amountToSend1, uint amountToSend2);
+    
     function Splitter() public {
         owner = msg.sender; 
     }
@@ -12,6 +16,7 @@ contract Splitter {
         uint amount = pendingWithdrawals[msg.sender];
         require (amount >= 0);
         pendingWithdrawals[msg.sender] = 0; 
+        LogWithdrawl(msg.sender, amount);
         msg.sender.transfer(amount);
     }
 
@@ -19,9 +24,15 @@ contract Splitter {
         require (msg.value % 2 == 0);
         uint amountToSend1 = msg.value/2;
         uint amountToSend2 = msg.value/2;
-        pendingWithdrawals[recipient1] = amountToSend1;
-        pendingWithdrawals[recipient2] = amountToSend2;
+        pendingWithdrawals[recipient1] += amountToSend1;
+        pendingWithdrawals[recipient2] += amountToSend2;
+        LogTransferSharedEther(recipient1, recipient2, amountToSend1, amountToSend2);
     } 
+    
+    function changeOwner(address newOwner) public onlyOwner {
+        if onlyOwner = owner;
+        owner = newOwner;
+    }
 
     function killMe() public returns (bool) {
         require(msg.sender == owner);
